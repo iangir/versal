1a93c6b515db4d2e17c6d69a27df039edab92f49
Raushan <mandarinko999@gmail.com>
2026-09-15 12:52:38 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://iangir.github.io',
-	base: '/versal-astro',
+	base: '/versal',
 });
